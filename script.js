bad655314355a8ddf3661c0f11d973dc79f09d33
MNice410 <mnice@nyit.edu>
2025-04-10 13:58:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     
     revealButton.addEventListener("click", () => {
-        hiddenMessage.style.display = "bloc"; 
-        revealButton.style.display = "none"
+        hiddenMessage.style.display = "block"; 
+        revealButton.style.display = "none";
     });
 });
